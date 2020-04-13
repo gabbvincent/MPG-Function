1d97629a6fcd847bbf5f0c67d1904e5c1a078017
Vincent Gabb <vincentgabb@gmail.com>
2020-04-13 20:23:32 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+//Name: Vincent G.
+//Date: 4/13/2020
+//Descritpion: MPG
+
+
+
+package main
+
+import "fmt"
+//Create a function to calculate MPG based off of gas used and miles driven.
+func MPG(a, b int)(string, int, string){
+//declare variable for MilesperGallon as int
+var MilesperGallon int
+MilesperGallon = a / b
+
+return "you got", MilesperGallon,"Miles per gallon!"
+}
+
+
+func main() {
+  var a int
+  var b int
+//ask user for miles driven and gallons used
+ fmt.Println("Enter in the amount of miles driven")
+ fmt.Scanln(&a)
+ fmt.Println("Enter in how many gallons of gas was used")
+ fmt.Scanln(&b)
+//call MPG(a, b)
+  fmt.Println(MPG(a,b))
+}
